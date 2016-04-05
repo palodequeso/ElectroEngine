26bb36b4b6806ebe4f6eb6c54df9dff320065292
unknown <palodequeso@gmail.com>
2016-04-05 18:54:14 -0400

    Started in on test game data

--- test_game/index.js
+++ test_game/index.js
@@ -1,12 +1,15 @@
 'use strict';
 
+var path = require("path");
 var $ = require("jquery");
 var GameModel = require('../engine/models/game.js');
 var Renderer = require('../engine/views/renderer.js');
 var Game = require('../engine/views/game.js');
+var gameio = require('../editor/gameio.js');
 
 document.addEventListener("DOMContentLoaded", () => {
-    var game_model = new GameModel({});
+    // var game_model = new GameModel({});
+    var game_model = gameio.load(path.normalize("test_game/data"));
     var game = new Game({
         model: game_model
     });
